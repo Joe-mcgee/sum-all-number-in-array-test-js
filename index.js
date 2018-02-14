42ed50f8fd55a17611e8d804994a4e326c1ac1ac
Taylor <thulsmans.2994@me.com>
2018-02-14 00:14:07 +0000

    added function that sums values of arbitrary nesting

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+function burrower(array) {
+  return array.reduce((a, b) => {
+    return a.concat(Array.isArray(b) ? burrower(b) : b);
+  }, []);
 }
 
+
+function sumItems(array) {
+  return burrower(array).reduce((a, b) => {
+    return a + b
+  }, 0);
+}
 module.exports = sumItems;
+
+
+
+console.log(burrower([[1, 2, [[3], 4]], 5, []]))
